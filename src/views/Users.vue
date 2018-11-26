d0679cf837e20d95c1cab5506a63f3a7d15af5b6
Nisarat Saelim <nisaratnsaelim@gmail.com>
2018-11-26 16:45:21 +0700

    add userlist

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,21 +1,21 @@
-  <template>
+<template>
   <div>
     <h1>User List</h1>
     <HelloWorld :msg="message"></HelloWorld>
-   
+    <!-- <HelloWorld></HelloWorld> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
 export default {
-    name: 'products',
+    name: 'users',
     components: {
     HelloWorld
   },
   data(){
     return{
-      message:'Product List'
+      message:'Project2'
     }
   }
 }
