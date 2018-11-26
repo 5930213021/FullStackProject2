7a4a97f3340be0a31f4740c7aa65323ac99b7dca
Nisarat Saelim <nisaratnsaelim@gmail.com>
2018-11-26 16:26:38 +0700

    add data and script of page products

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        <h1>Product List</h1>
-    </div>
+  <div>
+    <h1>Product List</h1>
+    <HelloWorld></HelloWorld>
+    <HelloWorld></HelloWorld>
+  </div>
 </template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+    name: 'products',
+    components: {
+    HelloWorld
+  }
+}
+</script>
